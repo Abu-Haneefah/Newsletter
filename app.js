7c6f0ecad24c94a7a22e100e1d6d7fbbf78ad718
Abdfatahi <aabdfatahi@gmail.com>
2024-01-02 19:33:30 +0100

    added git ignore files

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const request = require('request');
 const https = require('https');
 const mailchimp = require('mailchimp-api-v3');
 const app = express();
+ const dotenv = require('dotenv');
+ dotenv.config();
+
 // DYNAMIC HEROKU PORT
 const port = process.env.PORT || 3200;
 
@@ -12,6 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 // Display the sign Up form
+
 app.get('/', function(req,res){
   res.sendFile(__dirname + '/public/signUp.html')
 });
@@ -21,6 +25,11 @@ app.post('/', function(req,res){
   const firstName = req.body.fname;
   const lastname = req.body.lname;
   const email = req.body.email;
+
+  const apiKey = process.env.API_KEY;
+  const listId = process.env.LIST_ID;
+
+  console.log(apiKey,listId);
 
 // This is the data we want to send to mailchimp
   const data = {
@@ -35,13 +44,14 @@ app.post('/', function(req,res){
       }
     ]
   };
+
   // SINCE WE NEED A FLAT PACK JSON - like a straight line json
     const jsonData = JSON.stringify(data);
   // TO MAKE OUR REQUEST TO SEND/POST TO EXTERNAL
     const url = 'https://us9.api.mailchimp.com/3.0/lists/d1c528071c';
     const options = {
       method:'POST',
-      auth:'abdfatahi:c02e51f11513ab02fc7cf036d06060bf-us9',
+      auth:'abdfatahi:' + apiKey,
     };
    const request = https.request(url, options, function(response){
      if(response.statusCode === 200){
@@ -58,6 +68,12 @@ app.post('/', function(req,res){
       request.end();
 });
 
+app.post("/success", function(req,res){
+  res.redirect("/")
+});
+app.post("/failure", function(req,res){
+  res.redirect("/")
+});
 
 app.listen(port, function(){
   console.log('Server has started running on port 3200');
